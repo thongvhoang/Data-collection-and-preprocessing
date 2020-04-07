e276758025aac941cec7b7c210a65eac361d9fc1
thongvhoang <vhthongvn@gmail.com>
2020-04-08 00:06:07 +0700

    update

--- Crawl-corona-virus-data/worldometers_crawl/worldometers_crawl/spiders/process_corona_data.py
+++ Crawl-corona-virus-data/worldometers_crawl/worldometers_crawl/spiders/process_corona_data.py
@@ -1,5 +1,3 @@
-#f = open('new-cases-and-new-recovered-data-indonesia.txt','r')
-#f = open('total-cases-data-indonesia.txt', 'r')
 class process_data:
     def __init__(self,string_data):
         self.string_data = string_data
@@ -12,21 +10,13 @@ class process_data:
         Date_index_end = self.string_data.find('"]')
         Date = self.string_data[Date_index_start:Date_index_end].split('","')
         return Date#self.string_data[Date_start:Date_end]
+    
     def getNumberData(self):
         NumberData_index_start = self.string_data.find('data: [')+7
         NumberData_index_rstart = self.string_data.rfind('data: [')+7
-        print(NumberData_index_start,' ',NumberData_index_rstart)
         if (NumberData_index_start==NumberData_index_rstart or self.string_data.find('Total Cases')>0):
             NumberData_index_end = self.string_data.find('responsive')-23
         else:
             NumberData_index_end = self.string_data.rfind('color')-65
-            print(NumberData_index_end)
-        NumberData = self.string_data[NumberData_index_start:NumberData_index_end].split(',')        
-        
+        NumberData = self.string_data[NumberData_index_start:NumberData_index_end].split(',')                
         return NumberData
-
-#str = f.read()
-#a = process_data(str)
-#print(a.getNumberData())
-#print(process_data('<h3>Total Coronavirus Cases in Indonesia</h3>').getTitle())
-
--- Crawl-corona-virus-data/worldometers_crawl/worldometers_crawl/spiders/worldometers_spider.py
+++ Crawl-corona-virus-data/worldometers_crawl/worldometers_crawl/spiders/worldometers_spider.py
@@ -5,55 +5,51 @@ import scrapy #from scrapy import Spider
 import numpy as np
 from .process_corona_data import process_data #https://blog.teko.vn/2019/02/01/bay-import-trong-python/
 import pandas as pd
+from influxdb import InfluxDBClient
+from time import time
 
 
 class WorldometersSpider(scrapy.Spider): # from scrapy import Spider -> Spider
     name = "worldometers" # Define name of Spider
-
+        
     def start_requests(self):
 
         #   urls: List of urls are scrapped by Spider. Spider downloads all of urls's data.
         urls = [
             'https://www.worldometers.info/coronavirus/country/indonesia/',
-            # 'https://www.worldometers.info/coronavirus/country/philippines/',
+            'https://www.worldometers.info/coronavirus/country/philippines/',
         ]
         
         
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
-
+    
     def parse(self, response):
+        
         page = response.url.split("/")[-2]
         filename = 'coronavirus-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
-        
+        db = InfluxDBClient("localhost", 8086)
+        db.switch_database("worldometers")
+
+
+
         total_cases_data = response.xpath("//div//script[@type='text/javascript']")[2].get() # Don't use "" in @atribute=''
         daily_newcases_data = response.xpath("//div//script[@type='text/javascript']")[3].get() 
         daily_newdeaths_data = response.xpath("//div//script[@type='text/javascript']")[6].get()
         new_cases_and_new_recovered_data = response.xpath("//div//script[@type='text/javascript']")[7].get()
         titles_data = response.xpath("//h3").getall()
         
-        # f = open('total-cases-data-%s.txt'%page ,'w')
-        # f.write(total_cases_data)
-        # f.close()
-
-        # f = open('daily-newdeaths-data-%s.txt'%page ,'w')
-        # f.write(daily_newdeaths_data)
-        # f.close()
-
-        # f = open('new-cases-and-new-recovered-data-%s.txt'%page ,'w')
-        # f.write(new_cases_and_new_recovered_data)
-        # f.close()
-
-        
-        #crimes_rates = {"year":titles_data,"Population":[179323175,182992000,185771000,188483000,191141000],"Total":[3384200,3488000,3752200,4109500,4564600],"Violent":[288460,289390,301510,316970,364220]}
-        dataset = pd.DataFrame(data=process_data(total_cases_data).getDate(), columns=['Date'])
-        dataset['Country'] = page.title()
-        dataset['Total cases'] = process_data(total_cases_data).getNumberData()
-        dataset['Daily new deaths'] =  process_data(daily_newdeaths_data).getNumberData()
-        dataset['Daily new cases'] = process_data(daily_newcases_data).getNumberData()
-        dataset['Daily new recoveries'] = process_data(new_cases_and_new_recovered_data).getNumberData()
-        # print(dataset)
-        dataset.to_csv('coronavirus.csv',encoding='utf-8', index=False)
+        dataset_list = {"Date":process_data(total_cases_data).getDate(),
+                        "Country": [page.title() for i in range(len(process_data(total_cases_data).getDate()))],
+                        "Total cases": process_data(total_cases_data).getNumberData(),
+                        "Daily new deaths": process_data(daily_newdeaths_data).getNumberData(),
+                        "Daily new recoveries": process_data(new_cases_and_new_recovered_data).getNumberData()}
+        dataset = pd.DataFrame(data=dataset_list)
+        dataset.index = [i+1 for i in range(len(dataset))]
+        # dataset.to_csv('coronavirus.csv',encoding='utf-8', index=False)
+        yield dataset
+    def Res():
+        dataset = pd.DataFrame()
+        for data in parse(self,response):
+            dataset.append(data)
+        print(dataset)
